acef631e12f424612ab27aab38f8ef2bc6f7a495
pmherrar01 <pmherrerar01@educarex.es>
2024-02-27 18:31:30 +0100

    Ejercicio5repaso

--- Ejercicio5repaso/src/Ejercicio5.java
+++ Ejercicio5repaso/src/Ejercicio5.java
@@ -10,11 +10,11 @@ public class Ejercicio5 {
 		return numeros;		
 	}
 
-	public static void rellenarMatric(int numeros, int aNumeros[][]) {
+	public static void rellenarMatric(int aNumeros[][]) {
 		int i, j;
 		for(i=0;i<aNumeros.length;i++) {
-			for(j=0;j<aNumeros[j].length;j++) {
-				aNumeros[i][j] = numeros;
+			for(j=0;j<aNumeros[i].length;j++) {
+				aNumeros[i][j] = pedirNumeros();
 			}
 		}
 	}
@@ -22,43 +22,68 @@ public class Ejercicio5 {
 	public static void mostrarMatric(int aNumeros[][]) {
 		int i, j;
 		for (i=0; i<aNumeros.length;i++) {
-			for(j=0;j<aNumeros[j].length;j++) {
-				System.out.println(" [" + i + j + "] " + aNumeros[i][j]);
+			for(j=0;j<aNumeros[i].length;j++) {
+				System.out.print("["+aNumeros[i][j]+"]");
 			}
 		}
 	}
-	
-	public static int mostrarMayor(int aNumeros[][]) {
+
+	public static int calcularMayor(int aNumeros[][]) {
 		int mayor = aNumeros[0][0];
-        for(int i = 0;i < aNumeros.length;i++){
-            for(int j = 0; j < aNumeros[0].length;j++){
-                if  (aNumeros[i][j] > mayor){
-                    mayor = aNumeros[i][j];
-                }
-            }
-        }
-		
+		for(int i = 0;i < aNumeros.length;i++){
+			for(int j = 0; j < aNumeros[i].length;j++){
+				if  (aNumeros[i][j] > mayor){
+					mayor = aNumeros[i][j];
+				}
+			}
+		}
+
 		return mayor;
-		
+
 	}
-	
-	public static int mostrarMenor(int aNumeros[][]) {
+
+	public static int calcularMenor(int aNumeros[][]) {
 		int menor = aNumeros[0][0];
-        for(int i = 0;i < aNumeros.length;i++){
-            for(int j = 0; j < aNumeros[0].length;j++){
-                if  (aNumeros[i][j] < menor){
-                    menor = aNumeros[i][j];
-                }
-            }
-        }
-		
+		for(int i = 0;i < aNumeros.length;i++){
+			for(int j = 0; j < aNumeros[i].length;j++){
+				if  (aNumeros[i][j] < menor){
+					menor = aNumeros[i][j];
+				}
+			}
+		}
+
 		return menor;
-		
+
 	}
+
+	public static int calcularSuma(int aNumeros[][]) {
+	    int i, j, suma = 0;
+	    for (i = 0; i < aNumeros.length; i++) {
+	        for (j = 0; j < aNumeros[i].length; j++) {
+	            suma += aNumeros[i][j];
+	        }
+	    }
+	    return suma;
+	}
+
 
 	public static void main(String[] args) {
+		    int numeros = pedirNumeros();
+		    int[][] aNumeros = new int[4][2];
 
-		int [][] aNumeros = new int [4][2];
-	}
+		    rellenarMatric(aNumeros);
+		    mostrarMatric(aNumeros);
+
+		    int mayor, menor, suma;
+
+		    mayor = calcularMayor(aNumeros);
+		    menor = calcularMenor(aNumeros);
+		    suma = calcularSuma(aNumeros);
+
+		    System.out.println("El mayor es: " + mayor);
+		    System.out.println("El menor es: " + menor);
+		    System.out.println("La suma es: " + suma);
+		}
+
 
 }
